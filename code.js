42fb04cd97d2bb1beeff5d17754953055c883644
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-12-05 11:56:19 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,41 @@
 function tsp_ls(distance_matrix) {
-    return -1;
+    let n = distance_matrix.length;
+    if (n <= 1) return 0;
+
+    let currentRoute = [...Array(n).keys()];
+    shuffleArray(currentRoute);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function factorial(n) { //This is the factorial part of the bottomUp function done in dynamic-euler
+    if (n === 0) return 1;
+
+    let fact = 1;
+    for (let i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    return fact;
+}
+
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
